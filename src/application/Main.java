cd275d9c6e7e420dd7c32f2e320c61b9e71efd03
Harry Peach <harrypeach1999@gmail.com>
2018-10-20 14:56:11 +0100

    Added JRAW library

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,12 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
+import javafx.stage.Stage;
+import net.dean.jraw.http.UserAgent;
+import net.dean.jraw.oauth.Credentials;
 
 public class Main extends Application {
 	@Override
@@ -16,11 +17,16 @@ public class Main extends Application {
 			primaryStage.setTitle("RedditMonitor");
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		UserAgent userAgent = new UserAgent("bot", "application.redditmonitor", "0.1", "JellyGiant");
+		// MyCredentials is a class that contains static constants for sensitive data.
+		// It is hidden from the git repository.
+		Credentials credentials = Credentials.script(MyCredentials.username, MyCredentials.password,
+				MyCredentials.clientId, MyCredentials.secret);
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
