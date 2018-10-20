629f889deae190f7d5ac90d236afe815dd588613
Harry Peach <harrypeach1999@gmail.com>
2018-10-20 15:20:57 +0100

    Advances on UI handlers

--- src/application/FXMLCoreController.java
+++ src/application/FXMLCoreController.java
@@ -5,10 +5,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
 public class FXMLCoreController {
-    @FXML private ListView<String> postList;
+    @FXML private ListView<Result> postList;
     
     @FXML protected void handleSampleButton(ActionEvent event) {
-    	postList.setVisible(false);
+    	postList.getItems().add(new Result("Subreddit", "Title", "URL", "shortURL"));
     }
 
 }
