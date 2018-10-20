111fbab700c8c9e850091213bb023be8f49e9c66
Harry Peach <harrypeach1999@gmail.com>
2018-10-20 15:41:30 +0100

    Add gradle system

---
+++ src/main/java/MyCredentials.java
@@ -0,0 +1,14 @@
+/**
+ * Credentials class used for storing sensitive information
+ * @author harry
+ *
+ */
+
+public class MyCredentials {
+	
+	public static final String username = "JellyGiant";
+	public static final String password = "Ce7ofaKiDJlWh1E";
+	public static final String clientId = "JHcc97TbB0lPZg";
+	public static final String secret = "vHR6TNWcHtLxY5pe8OHZmjJ2qsQ";
+
+}
