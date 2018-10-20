88ae2d319dbc6d167d70e9bbd2ce33e6f7c26ae3
Harry Peach <harrypeach1999@gmail.com>
2018-10-20 21:53:45 +0100

    Implement autoscroll when adding new items

--- src/main/java/FXMLCoreController.java
+++ src/main/java/FXMLCoreController.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -11,7 +15,6 @@ import net.dean.jraw.models.Submission;
 import net.dean.jraw.models.SubredditSort;
 import net.dean.jraw.pagination.DefaultPaginator;
 import net.dean.jraw.references.SubredditReference;
-
 
 public class FXMLCoreController {
 	@FXML
@@ -23,28 +26,31 @@ public class FXMLCoreController {
 
 	@FXML
 	protected void handleStartButton(ActionEvent event) {
-		if(t.getState().equals(Thread.State.NEW)) {
+		if (t.getState().equals(Thread.State.NEW)) {
 			t.start();
 		}
 		threadEnabled = !threadEnabled;
-
+		String buttonText = (startButton.getText().equals("Start"))
+				? ("Stop")
+				: ("Start");
+		startButton.setText(buttonText);
 	}
-	
+
 	@FXML
 	protected void initialize() {
 		t = new Thread(new UpdateList(this));
 		t.setDaemon(true);
 	}
-	
+
 	@FXML
 	public void exitApplication(ActionEvent event) {
-	    threadEnabled = false;
+		threadEnabled = false;
 	}
 
 	public ListView<Result> getPostList() {
 		return postList;
 	}
-	
+
 	public void shutdown() {
 		threadEnabled = false;
 	}
@@ -54,6 +60,8 @@ public class FXMLCoreController {
 class UpdateList implements Runnable {
 	private FXMLCoreController controllerInstance;
 	Queue<Result> resultQueue = new LinkedList<>();
+	List<String> stringList = Arrays.asList("steam key", "giving away", "giveaway", "cd key", "steam code", "cd code",
+			"spare code", "spare key", "extra key", "extra code", "give away", "the");
 
 	public UpdateList(FXMLCoreController instance) {
 		controllerInstance = instance;
@@ -61,25 +69,30 @@ class UpdateList implements Runnable {
 
 	public void run() {
 		SubredditReference all = Main.redditHelper.getRedditClient().subreddit("all");
-		
+
 		while (true) {
-			if(controllerInstance.threadEnabled) {
+			if (controllerInstance.threadEnabled) {
 				try {
 					DefaultPaginator<Submission> paginator = all.posts().sorting(SubredditSort.NEW).limit(30).build();
 					Listing<Submission> submissions = paginator.next();
-					
-					for(Submission s : submissions) {
+
+					for (Submission s : submissions) {
 						Result r = new Result(s.getSubreddit(), s.getTitle(), s.getUrl(), s.getId());
-						if(!containsResult(resultQueue, r)) {
+						if (titleContainsWordList(r.getTitle(), stringList) && !containsResult(resultQueue, r)) {
 							addToQueue(r);
-							controllerInstance.getPostList().getItems().add(r);
+							Runnable updater = new Runnable() {
+								public void run() {
+									controllerInstance.getPostList().getItems().add(r);
+								}
+							};
+							Platform.runLater(updater);
 						}
-					}				
+					}
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-			}else {
+			} else {
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
@@ -88,22 +101,37 @@ class UpdateList implements Runnable {
 			}
 		}
 	}
-
-	private boolean containsResult(Queue<Result> resultQueueIn, Result testResult) {
-		for(Result result : resultQueueIn) {
-			if(result.getId().equals(testResult.getId())) {
+	
+	private boolean titleContainsWordList(String word, List<String> wordList) {
+		for(String s : wordList) {
+			if(word.toLowerCase().contains(s)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
+	private boolean containsResult(Queue<Result> resultQueueIn, Result testResult) {
+		for (Result result : resultQueueIn) {
+			if (result.getId().equals(testResult.getId())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	private void addToQueue(Result r) {
-		if(resultQueue.size() >= 100) {
+		if (resultQueue.size() >= 100) {
 			resultQueue.remove();
 			resultQueue.add(r);
-		}else {
+		} else {
 			resultQueue.add(r);
 		}
+		Runnable updater = new Runnable() {
+			public void run() {
+				controllerInstance.getPostList().scrollTo(controllerInstance.getPostList().getItems().size());
+			}
+		};
+		Platform.runLater(updater);
 	}
 }
