f3bd28e446ea04163ef62f6412f48b75b407cc72
Harry Peach <harrypeach1999@gmail.com>
2018-11-08 16:43:40 +0000

    Log reader now reads and displays current log file

--- src/main/java/uk/co/harrypeach/ui/LogReaderDialog.java
+++ src/main/java/uk/co/harrypeach/ui/LogReaderDialog.java
@@ -1,5 +1,16 @@
 package uk.co.harrypeach.ui;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javafx.geometry.Insets;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
@@ -12,6 +23,7 @@ import javafx.scene.layout.VBox;
 public class LogReaderDialog extends Dialog<String>{
 	
 	private ListView<String> logList = new ListView<>();
+	private static final Logger LOGGER = LoggerFactory.getLogger(LogReaderDialog.class);
 	
 	public LogReaderDialog() {
 		setTitle("Logs");
@@ -26,6 +38,31 @@ public class LogReaderDialog extends Dialog<String>{
 		vbox.setPadding(new Insets(16));
 		
 		getDialogPane().setContent(vbox);
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		String currentFormattedDate = formatter.format(date);
+		String logFileName = "log/RedditMonitor." + currentFormattedDate + ".log";
+		File f = new File(logFileName);
+		if(f.exists() && !f.isDirectory()) {
+			try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+			    String line;
+			    while ((line = br.readLine()) != null) {
+			       logList.getItems().add(line);
+			    }
+			} catch (FileNotFoundException e) {
+				LOGGER.error("FileNotFound Exception when accessing log file");
+				LOGGER.error(e.getMessage());
+				e.printStackTrace();
+			} catch (IOException e) {
+				LOGGER.error("IO Exception when accessing log file");
+				LOGGER.error(e.getMessage());
+				e.printStackTrace();
+			}
+		}else {
+			logList.getItems().add("Log could not be loaded");
+			LOGGER.warn("Log file could not be loaded as it does not exist");
+		}
 		
 		ButtonType okButton = new ButtonType("Done", ButtonBar.ButtonData.OK_DONE);
 		getDialogPane().getButtonTypes().addAll(okButton, ButtonType.CANCEL);
